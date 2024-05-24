86892ebd298c641812781b6598a9cf18c8976776
WizzardOff <Timur@MacBook-Pro.local>
2024-05-25 00:41:40 +0300

    v3 of algorithm raw ready, added goroutines into the flow

--- main.go
+++ main.go
@@ -46,25 +46,93 @@ func checkPaletteIsValid(palette *Palette) {
 	}
 }
 
-func placeItemsOnPalette(palette Palette, item Item, chanel chan<- ResultPlacement) {
+/*
+*
+The main function that is used in order to place items on palette
+*/
+func placeItemsOnPalette(palette Palette, item *Item, chanel chan<- ResultChanLayers) {
+	var layers []Layer
+	var totalItems int
+	totalVolume := palette.Width * palette.Height * palette.Depth
+	hasBeenRotated := false
+	/**
+	Check if one of the item sides is close to pallete size in proximity of 15%
+	*/
+	RotationIn3d(item, palette.Width, palette.Height, palette.Depth)
 	/**
 	Check if values provided are ok, e.g no zero values provided
 	*/
-	validateInputValues(&palette, &item)
+	validateInputValues(&palette, item)
 	/**
 	We can then check if volume of item and palette is close to each other , eg
 	volume of palette 1600 and item is 1300, then of course we can only place one item
 	*/
 	/**
-	We can move through palette width
+	We can move through palette depth,width and height
 	*/
-	for i := 0; i <= palette.Width; i += item.Width {
-		fmt.Println("Im at", i)
+	for z := 0; z <= palette.Depth; z += item.Depth {
+		remainingHeight := palette.Height
+		var layerItems []Coordinate
+		for y := 0; y < palette.Height; y += item.Height {
+			if remainingHeight < item.Height {
+				RotateItem(item)
+				hasBeenRotated = true
+				if remainingHeight < item.Height {
+					break
+				}
+			}
+			remainingHeight -= item.Height
+			remainingWidth := palette.Width
+			for x := 0; x < palette.Width; x += item.Width {
+				// test also if height is left check volume of space left and try to fit
+				if (remainingWidth < item.Width) && remainingHeight == 0 {
+					break
+				} else if (remainingWidth < item.Width) && remainingHeight > 0 {
+					if !hasBeenRotated {
+						RotateItem(item)
+						hasBeenRotated = true
+					}
+					if remainingHeight*item.Width < item.Width*item.Height {
+						break
+					}
+				}
+				remainingWidth -= item.Width
+
+				if x+item.Width <= palette.Width && y+item.Height <= palette.Height && z+item.Depth <= palette.Depth {
+					layerItems = append(layerItems, Coordinate{X: x, Y: y, Z: z, ItemDepth: item.Depth, ItemWidth: item.Width, ItemHeight: item.Height})
+					totalItems++
+				}
+			}
+			if hasBeenRotated {
+				RotateItem(item)
+				hasBeenRotated = false
+			}
+		}
+		if hasBeenRotated {
+			RotateItem(item)
+			hasBeenRotated = false
+		}
+		if len(layerItems) > 0 {
+			itemOneVolume := item.Width * item.Height
+			itemAllVolume := len(layerItems) * itemOneVolume
+			paletteVolume := palette.Width * palette.Height
+			if paletteVolume-itemAllVolume > itemOneVolume {
+				rearrangeLayer(layerItems, item, palette.Width, palette.Height)
+			}
+		}
+		layers = append(layers, Layer{Items: layerItems})
+
 	}
-	chanel <- ResultPlacement{
-		Layers: nil,
-		Volume: 0,
-	}
+	volumeUsage := float64(totalItems*item.Width*item.Height*item.Depth) / float64(totalVolume) * 100.0
+	/**
+	As we are working with gorotuines we need to push data to channels
+	*/
+	chanel <- struct {
+		Layers      []Layer
+		VolumeUsed  float64
+		ItemsPlaced int
+		PaletteUsed Palette
+	}{Layers: layers, VolumeUsed: volumeUsage, ItemsPlaced: totalItems, PaletteUsed: palette}
 }
 
 /*
@@ -84,39 +152,183 @@ func main() {
 		Start palette placement in 3 different go routines and wait for all to complete
 		Capture results in Channel
 	*/
-	resultChan := make(chan ResultPlacement, 3)
+	resultChan := make(chan ResultChanLayers, 18)
 	palettePlans := GetPalettePlans()
+	/**
+	WaitGroup is used in order to sync separate goroutines
+	*/
 	wgMain := sync.WaitGroup{}
 	wgMain.Add(len(palettePlans))
 	for _, palette := range palettePlans {
 		// create intermediary variable 'palette' , otherwise we can end up with unexpected results!!!
 		palette := palette
-		go func(channel chan<- ResultPlacement) {
-			placeItemsOnPalette(palette, Item{
-				Width:  2,
-				Height: 6,
-				Depth:  2,
-				X:      0,
-				Y:      0,
-				Z:      0,
-			}, resultChan)
+		go func(channel chan<- ResultChanLayers) {
+			/**
+			W D H
+			Original - 1 2 3
+			- Change width and depth  - 2 1 3
+			- Change width and height  - 3 2 1
+			- Change depth and height - 1 3 2
+			- Change width and depth and depth and height 2 3 1
+			- Change depth and width and width and height  3 1 2
+			Original + 5 possible rotations
+			*/
+			item := Item{
+				Width:  600,
+				Height: 400,
+				Depth:  100,
+			}
+			/**
+			we need syncronization on this level as well
+			We are rotating each item and finding the best placement
+			*/
+			childWg := sync.WaitGroup{}
+			for i := 0; i < 6; i++ {
+				childWg.Add(1)
+				i := i
+				go func() {
+					if i > 0 {
+						item.Rotate(i)
+					}
+					defer childWg.Done()
+					placeItemsOnPalette(palette, &item, resultChan)
+				}()
+			}
+			childWg.Wait()
 			defer wgMain.Done()
 		}(resultChan)
 	}
 	wgMain.Wait()
 	close(resultChan)
 
+	var resSlice []ResultChanLayers
+	/**
+	The chanSelect below iterates over result channel
+
+	resultChan - channel that hold results from each goroutine
+	goroutine operates in async nature and places result in channel, so we can act on it
+	*/
 chanSelect:
 	for {
 		select {
 		case data, ok := <-resultChan:
 			if ok {
-				fmt.Println(data)
+				resSlice = append(resSlice, data)
 			} else {
 				break chanSelect
 			}
+			/**
+			default cases needed when no data added
+			*/
 		default:
-			//do nothing..
+			// no item placed in here
+		}
+	}
+	/**
+	This hold all placements available for different palettes
+	e.g.
+	euro palette placement
+	industrial palette placement
+	half palette placement
+
+	Based on information in each slice we can find best placement for each palette , we can use items placed and volume used
+	*/
+
+	palettePlansMap := GetPalettePlans()
+	var sliceEuroPalette []ResultChanLayers
+	var sliceIndustrialPalette []ResultChanLayers
+	var sliceHalfPalette []ResultChanLayers
+
+	var bestEuroPalettePlacement ResultChanLayers
+	var sliceIndustrialPalettePlacement ResultChanLayers
+	var bestSilceHalfPalettePlacement ResultChanLayers
+
+	/**
+	Print placement here...
+	*/
+	for _, placement := range resSlice {
+		if placement.PaletteUsed == palettePlansMap["EuroPalette"] {
+			sliceEuroPalette = append(sliceEuroPalette, placement)
+			if placement.VolumeUsed > bestEuroPalettePlacement.VolumeUsed && placement.ItemsPlaced > bestEuroPalettePlacement.ItemsPlaced {
+				bestEuroPalettePlacement = placement
+			}
+		}
+		if placement.PaletteUsed == palettePlansMap["IndustrialPalette"] {
+			sliceIndustrialPalette = append(sliceIndustrialPalette, placement)
+			if placement.VolumeUsed > sliceIndustrialPalettePlacement.VolumeUsed && placement.ItemsPlaced > sliceIndustrialPalettePlacement.ItemsPlaced {
+				sliceIndustrialPalettePlacement = placement
+			}
+		}
+		if placement.PaletteUsed == palettePlansMap["HalfPalette"] {
+			sliceHalfPalette = append(sliceHalfPalette, placement)
+			if placement.VolumeUsed > bestSilceHalfPalettePlacement.VolumeUsed && placement.ItemsPlaced > bestSilceHalfPalettePlacement.ItemsPlaced {
+				bestSilceHalfPalettePlacement = placement
+			}
+		}
+	}
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Euro Pall")
+	fmt.Printf("Palette used - %v \n", bestEuroPalettePlacement.PaletteUsed)
+	for i, layer := range bestEuroPalettePlacement.Layers {
+		if len(layer.Items) > 0 {
+			fmt.Println("layer ", i)
+			fmt.Println(layer.Items)
+		}
+	}
+	fmt.Printf("Items placed %d ", bestEuroPalettePlacement.ItemsPlaced)
+	fmt.Printf("Volume used %f ", bestEuroPalettePlacement.VolumeUsed)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Industrial Pall")
+	fmt.Printf("Palette used - %v \n", sliceIndustrialPalettePlacement.PaletteUsed)
+	for i, layer := range sliceIndustrialPalettePlacement.Layers {
+		if len(layer.Items) > 0 {
+			fmt.Println("layer ", i)
+			fmt.Println(layer.Items)
+		}
+	}
+	fmt.Printf("Items placed %d ", sliceIndustrialPalettePlacement.ItemsPlaced)
+	fmt.Printf("Volume used %f ", sliceIndustrialPalettePlacement.VolumeUsed)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Half Pall")
+	fmt.Printf("Palette used - %v \n", bestSilceHalfPalettePlacement.PaletteUsed)
+	for i, layer := range bestSilceHalfPalettePlacement.Layers {
+		if len(layer.Items) > 0 {
+			fmt.Println("layer ", i)
+			fmt.Println(layer.Items)
+		}
+	}
+	fmt.Printf("Items placed %d ", bestSilceHalfPalettePlacement.ItemsPlaced)
+	fmt.Printf("Volume used %f ", bestSilceHalfPalettePlacement.VolumeUsed)
+}
+
+func rearrangeLayer(coordinates []Coordinate, item *Item, paletteWidth int, paletteHeight int) {
+	//rotate most right and try to fit --- find most right item
+	initialWidth := paletteWidth
+	// aggregate all width before last element
+	allWidthWithLast := 0
+	allHeightWithLast := 0
+	for i, coordinate := range coordinates {
+		allWidthWithLast += coordinate.ItemWidth
+		allHeightWithLast += coordinate.ItemHeight
+		initialWidth = initialWidth - coordinate.ItemWidth
+		if initialWidth <= 0 {
+			// last item in the row
+			lastInLayerRow := coordinates[i-1]
+			nextInLayerRow := coordinate
+			// remove last width from all and add height if rotated
+			swappedXYSummWidth := allWidthWithLast - lastInLayerRow.ItemWidth + lastInLayerRow.ItemHeight
+			swappedXYSummHeight := allHeightWithLast - lastInLayerRow.ItemHeight + lastInLayerRow.ItemWidth
+			if swappedXYSummWidth <= paletteWidth && swappedXYSummHeight <= paletteHeight {
+				// check that there is no overlap with item below
+				fmt.Println(nextInLayerRow)
+			}
+
 		}
 	}
 }
--- packTypes/types.go
+++ packTypes/types.go
@@ -9,13 +9,24 @@ type Palette struct {
 	Width, Height, Depth int
 }
 
+/*
+Result chan Layer struct needed to push items in chan
+*/
+type ResultChanLayers struct {
+	Layers      []Layer
+	VolumeUsed  float64
+	ItemsPlaced int
+	PaletteUsed Palette
+}
+
 type Item struct {
+	RotationCalls        int
 	Width, Height, Depth int
 	X, Y, Z              int
 }
 
 type Layer struct {
-	Items  []*Item
+	Items  []Coordinate
 	Height int
 }
 
@@ -56,7 +67,8 @@ func (item *Item) setDepth(depth int) {
 Function for rotating item dimensions based on rotation provided
 */
 
-func (item *Item) rotate(rotation int) *Item {
+func (item *Item) Rotate(rotation int) *Item {
+	item.RotationCalls = item.RotationCalls + 1
 	switch rotation {
 	case 1:
 		return &Item{Width: item.Depth, Depth: item.Height, Height: item.Width}
@@ -82,7 +94,7 @@ func GetPalettePlans() PaletteMap {
 		Depth:  800,
 	}
 
-	initMap["IndustriePalette"] = Palette{
+	initMap["IndustrialPalette"] = Palette{
 		Width:  1200,
 		Height: 1000,
 		Depth:  800,
@@ -95,6 +107,36 @@ func GetPalettePlans() PaletteMap {
 	}
 
 	return initMap
+}
+
+type Coordinate struct {
+	X, Y, Z                          int
+	ItemWidth, ItemHeight, ItemDepth int
+}
+
+// RotateItem rotates the item dimensions to maximize space utilization within the layer
+func RotateItem(item *Item) {
+	// Swap width and height
+	item.Width, item.Height = item.Height, item.Width
+}
+
+func RotationIn3d(item *Item, palWidth int, palHeight int, palDepth int) {
+	/*
+		Большую сторону надо двигать к наиболее большой стороне паллеты
+	*/
+	// need to rotate item height and depth
+	if item.Height > palHeight && item.Height <= palDepth && palHeight >= item.Depth {
+		itemDepth := item.Depth
+		item.Depth = item.Height
+		item.Height = itemDepth
+	}
+
+	// need to rotate item height and width
+	if item.Height > palHeight && item.Height <= palWidth && palHeight >= item.Width {
+		itemHeight := item.Height
+		item.Height = item.Width
+		item.Width = itemHeight
+	}
 }
 
 /**
